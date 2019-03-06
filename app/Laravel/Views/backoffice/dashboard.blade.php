fcf8ee80ba29fa2f274f12af4deb1cdcfb0334b6
tandelvijay <tandelvijay19@gmail.com>
2019-03-06 19:37:56 +0530

    all design Update

--- app/Laravel/Views/backoffice/dashboard.blade.php
+++ app/Laravel/Views/backoffice/dashboard.blade.php
@@ -1,68 +1,90 @@
 @extends('backoffice._layouts.app')
 @section('content')
 <div class="robust-content content container-fluid">
+	<section class="content-header">
+		<h1>Dashboard</h1>
+	</section>
 	<div class="content-wrapper">
-		<div class="content-header row">
-		</div>
+
 		<div class="content-body"><!-- Analytics charts -->
 			<div class="row">
+				<div class="col-md-8 col-md-lg-8 col-xs-8">
+					<div id="map" style="height: 400px; width: 100%;"></div>
+				</div>
 				<div class="col-xl-4 col-lg-12 col-md-12">
-			        <div class="card">
-	                    <div class="card-body">
-	                        <div class="card-block">
-	                            <div class="media">
-	                                <div class="media-body text-xs-left">
-	                                    <h3 class="success">{{$stations->count()}}</h3>
-	                                    <span>Active Stations</span>
-	                                    <small><a href="{{route('backoffice.station.index')}}">[Manage stations]</a></small>
-	                                </div>
-	                                <div class="media-right media-middle">
-	                                    <i class="icon-location3 success font-large-2 float-xs-right"></i>
-	                                </div>
-	                            </div>
-	                            {{-- <progress class="progress progress-sm progress-success mt-1 mb-0" value="80" max="100"></progress> --}}
-	                        </div>
-	                    </div>
-	                </div>
-			        <div class="card">
-			            <div class="card-body">
-			                <div class="card-block">
-			                    <div class="media">
-			                        <div class="media-body text-xs-left">
-			                            <h3 class="info">{{$customers->where('created_at','>=',$date_today)->count()}}</h3>
-			                            <span>Today's Registrant</span>
-			                        </div>
-			                        <div class="media-right media-middle">
-			                            <i class="icon-user5 info font-large-2 float-xs-right"></i>
-			                        </div>
-			                    </div>
-			                    {{-- <progress class="progress progress-sm progress-info mt-1 mb-0" value="35" max="100"></progress> --}}
-			                </div>
-			            </div>
-			        </div>
-			        <div class="card">
-			            <div class="card-body">
-			                <div class="card-block">
-			                    <div class="media">
-			                        <div class="media-body text-xs-left">
-			                            <h3 class="info">{{$customers->count()}}</h3>
-			                            <span>All App Users</span>
-			                        </div>
-			                        <div class="media-right media-middle">
-			                            <i class="icon-user5 info font-large-2 float-xs-right"></i>
-			                        </div>
-			                    </div>
-			                    {{-- <progress class="progress progress-sm progress-info mt-1 mb-0" value="35" max="100"></progress> --}}
-			                </div>
-			            </div>
-			        </div>
+					<div class="card-group">
+						<div class="card">
+							<div class="card-body text-center">
+								<div class="card-block">
+									 <div class="media ml-1">
+										<div class="  media-middle">
+											<i class="icon-location3 success font-large-2 text-center"></i>
+										</div>
+										<div class="media-body text-xs-center">
+											<span>Active Stations</span>
+											<small><a href="{{route('backoffice.station.index')}}">[Manage stations]</a></small>
+										</div>
+										<div class="media-bottom">
+											<h3 class="success">{{$stations->count()}}</h3>
+										</div>
+									</div>
+									{{-- <progress class="progress progress-sm progress-success mt-1 mb-0" value="80" max="100"></progress> --}}
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-body text-center">
+								<div class="card-block">
+									<div class="media ml-1">
+										<div class="media-middle">
+											<i class="icon-user5 info font-large-2"></i>
+										</div>
+										<div class="media-body text-xs-left">
+											<span>Today's Registrant</span>
+										</div>
+										<div class="media-bottom">
+											<h3 class="info">{{$customers->where('created_at','>=',$date_today)->count()}}</h3>
+										</div>
+									</div>
+									{{-- <progress class="progress progress-sm progress-info mt-1 mb-0" value="35" max="100"></progress> --}}
+								</div>
+							</div>
+						</div>
+						<div class="card">
+							<div class="card-body text-center">
+								<div class="card-block">
+									<div class="media ml-1">
+										<div class="media-middle">
+											<i class="icon-user5 info font-large-2"></i>
+										</div>
+										<div class="media-body text-xs-left">
+											<span>All App Users</span>
+										</div>
+										<div class="media-bottom">
+											<h3 class="info">{{$customers->count()}}</h3>
+										</div>
+									</div>
+									{{-- <progress class="progress progress-sm progress-info mt-1 mb-0" value="35" max="100"></progress> --}}
+								</div>
+							</div>
+						</div>
+					</div>
 			    </div>
-			    <div class="col-md-8 col-md-lg-8 col-xs-8">
-			    	<div id="map" style="height: 400px; width: 100%;"></div>
-			    </div>
+				{{--------------CHART SECTION----------------}}
+				<div class="col-xl-4 col-lg-12 col-md-12">
+					<div class="box box-danger">
+						<div class="box-body">
+							<div id="donutchart" style="width: 357px; height: 200px;"></div>
+						</div>
+						<!-- /.box-body -->
+					</div>
+			    {{--<div class="col-md-8 col-md-lg-8 col-xs-8">--}}
+			    	{{--<div id="map" style="height: 400px; width: 100%;"></div>--}}
+			    {{--</div>--}}
+			</div>
 			</div>
 			<div class="row">
-				<div class="col-lg-12">
+				<div class="col-md-8 col-md-lg-8 col-xs-8">
 					<div class="card">
 			            <div class="card-header">
 			                <h4 class="card-title">Today's Activity Report ({{Carbon::now()->format("M d Y")}})</h4>
@@ -130,16 +152,24 @@
 			            </div>
 			        </div>
 				</div>
+				<div class="col-xl-4 col-lg-12 col-md-12 mt-1">
+					<div class="box box-danger">
+						<div class="box-body">
+							<div id="donutchart1" style="width: 357px; height: 200px;"></div>
+						</div>
+						<!-- /.box-body -->
+					</div>
+				</div>
 			</div>
 			<div class="row">
-				<div class="col-lg-12">
+				<div class="col-md-8 col-md-lg-8 col-xs-8">
 					<div class="card">
 			            <div class="card-header">
 			                <h4 class="card-title">Registered Users</h4>
 			            </div>
 			            <div class="card-content">
 			                <div class="table-responsive">
-			                    <table class="table table-hover mb-0">
+			                    <table class="table no-margin">
 			                        <thead>
 			                            <tr>
 			                                <th>Name</th>
@@ -151,12 +181,12 @@
 			                        <tbody>
 			                        	@foreach($customers as $index => $user)
 			                            <tr>
-			                                <td class="text-truncate"><span><a href="{{route('backoffice.user.edit',[$user->id])}}">{{$user->name}}</a></span>
+			                                <td><span><a href="{{route('backoffice.user.edit',[$user->id])}}">{{$user->name}}</a></span>
 		                                	<div><small>{{ $user->email }}</small></div>
-		                                	<div><small>{{ $user->contact }}</small></div> 
+		                                	<div><small>{{ $user->contact }}</small></div>
 
 			                                </td>
-			                                <td class="text-truncate">
+			                                <td>
 			                                	<a href="{{route('backoffice.user.edit',[$user->id])}}">{{$user->num_farm}}</a>
 			                               	<div>
 			                               	  <small>
@@ -170,8 +200,8 @@
 			                               	  </small>
 			                               	</div>
 			                                </td>
-			                                <td class="text-truncate">{{$user->created_at->format("d M Y h:i A")}}</td>
-			                                <td class="text-truncate">{{Helper::time_passed($user->last_activity)}}</td>
+			                                <td>{{$user->created_at->format("d M Y h:i A")}}</td>
+			                                <td>{{Helper::time_passed($user->last_activity)}}</td>
 			                            </tr>
 			                            @endforeach
 			                        </tbody>
@@ -179,6 +209,14 @@
 			                </div>
 			            </div>
 			        </div>
+				</div>
+				<div class="col-xl-4 col-lg-12 col-md-12 mt-1">
+					<div class="box box-danger">
+						<div class="box-body">
+							<div id="donutchart2" style="width: 357px; height: 200px;"></div>
+						</div>
+						<!-- /.box-body -->
+					</div>
 				</div>
 			</div>
 			
@@ -251,6 +289,139 @@
               }
             })(marker, i));
           }
+
+
+	  var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
+	  var pieChart       = new Chart(pieChartCanvas)
+	  var PieData        = [
+		  {
+			  value    : 700,
+			  color    : '#f56954',
+			  highlight: '#f56954',
+			  label    : 'Chrome'
+		  },
+		  {
+			  value    : 500,
+			  color    : '#00a65a',
+			  highlight: '#00a65a',
+			  label    : 'IE'
+		  },
+		  {
+			  value    : 400,
+			  color    : '#f39c12',
+			  highlight: '#f39c12',
+			  label    : 'FireFox'
+		  },
+		  {
+			  value    : 600,
+			  color    : '#00c0ef',
+			  highlight: '#00c0ef',
+			  label    : 'Safari'
+		  },
+		  {
+			  value    : 300,
+			  color    : '#3c8dbc',
+			  highlight: '#3c8dbc',
+			  label    : 'Opera'
+		  },
+		  {
+			  value    : 100,
+			  color    : '#d2d6de',
+			  highlight: '#d2d6de',
+			  label    : 'Navigator'
+		  }
+	  ]
+	  var pieOptions     = {
+		  //Boolean - Whether we should show a stroke on each segment
+		  segmentShowStroke: true,
+		  //String - The colour of each segment stroke
+		  segmentStrokeColor: '#fff',
+		  //Number - The width of each segment stroke
+		  segmentStrokeWidth: 2,
+		  //Number - The percentage of the chart that we cut out of the middle
+		  percentageInnerCutout: 50, // This is 0 for Pie charts
+		  //Number - Amount of animation steps
+		  animationSteps: 100,
+		  //String - Animation easing effect
+		  animationEasing: 'easeOutBounce',
+		  //Boolean - Whether we animate the rotation of the Doughnut
+		  animateRotate: true,
+		  //Boolean - Whether we animate scaling the Doughnut from the centre
+		  animateScale: false,
+		  //Boolean - whether to make the chart responsive to window resizing
+		  responsive: true,
+		  // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+		  maintainAspectRatio: true,
+		  //String - A legend template
+		  {{--legendTemplate       : '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<segments.length; i++)' + '{%><li><span style="background-color:<%=segments[i].fillColor%>">' + '</span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>'--}}
+	  }
+
+    //Create pie or douhnut chart
+    // You can switch between pie and douhnut using the method below.
+    pieChart.Doughnut(PieData, pieOptions)
   });
+</script>
+{{------------------CHART SCRIPT-------------------}}
+
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script>
+	google.charts.load("current", {packages:["corechart"]});
+	google.charts.setOnLoadCallback(drawChart);
+	function drawChart() {
+		var data = google.visualization.arrayToDataTable([
+			['Task', 'Hours per Day'],
+			['Work',     8],
+			['Sleep',    9]
+		]);
+
+		var options = {
+			title: 'Harwest Window',
+			pieHole: 0.4,
+			colors: ['#FFC200', '#CCCCCC']
+		};
+
+		var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+		chart.draw(data, options);
+	}
+</script>
+<script>
+	google.charts.load("current", {packages:["corechart"]});
+	google.charts.setOnLoadCallback(drawChart);
+	function drawChart() {
+		var data = google.visualization.arrayToDataTable([
+			['Task', 'Hours per Day'],
+			['Work',     10],
+			['Sleep',    7]
+		]);
+
+		var options = {
+			title: 'Fertilizer Application',
+			pieHole: 0.4,
+			colors: ['#89C409', '#CCCCCC']
+		};
+
+		var chart = new google.visualization.PieChart(document.getElementById('donutchart1'));
+		chart.draw(data, options);
+	}
+</script>
+<script>
+	google.charts.load("current", {packages:["corechart"]});
+	google.charts.setOnLoadCallback(drawChart);
+	function drawChart() {
+		var data = google.visualization.arrayToDataTable([
+			['Task', 'Hours per Day'],
+			['Work',     16],
+			['Sleep',    6]
+		]);
+
+		var options = {
+			title: 'Pesticide Application',
+			pieHole: 0.4,
+			colors: ['#49AF35', '#CCCCCC']
+		};
+
+		var chart = new google.visualization.PieChart(document.getElementById('donutchart2'));
+		chart.draw(data, options);
+	}
 </script>
 @stop
