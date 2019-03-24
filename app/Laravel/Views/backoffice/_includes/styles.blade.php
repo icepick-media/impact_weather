9bcc44ad5bbff32096b0cc7315c2dccae7cf8a3c
Aneep Tandel <aneepct@live.com>
2019-03-25 00:14:54 +0530

    add fonts

--- app/Laravel/Views/backoffice/_includes/styles.blade.php
+++ app/Laravel/Views/backoffice/_includes/styles.blade.php
@@ -28,3 +28,4 @@
   <link rel="stylesheet" href="/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
 
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+  <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700,800,900" rel="stylesheet">
